4fbcac38c020b17c9154785a59f59458b3f886e2
reza <rezanaeimabadi7@gmail.com>
2022-12-26 07:44:17 -0700

    created swiper in the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'slider-app';
